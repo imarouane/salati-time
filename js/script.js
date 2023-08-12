8ed61eeae344977c9118c69541561b28769b0358
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-08-12 20:14:15 +0100

    update cities data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const loadingEl = document.querySelector(".loading");
 const imgEl = document.querySelector(".loading img");
-
+const citiesList = document.querySelector(".cities__list");
 setTimeout(() => {
   loadingEl.classList.add("anime");
   imgEl.classList.add("anime");
@@ -10,6 +10,15 @@ setTimeout(() => {
 }, 1000);
 
 axios.get("../assets/data/cities.json").then((response) => {
-  console.log(response.data[6].names.ar);
-  // return response.data;
+  const cities = response.data;
+  const sortedCeties = cities.sort((a, b) => {
+    return a === b ? 0 : a > b ? 1 : -1;
+  })
+  console.log(sortedCeties);
+  // for (const city of cities) {
+  //   const content = `
+  //   <li class="city" data-city-name="${city.en}">${city.ar}</li>
+  //   `;
+  //   citiesList.innerHTML += content;
+  // }
 });
