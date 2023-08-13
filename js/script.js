75c0853babc1b7b12b3fc248352ac3eec1179fa4
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-08-13 20:20:14 +0100

    add animations

--- js/script.js
+++ js/script.js
@@ -2,14 +2,17 @@ const loadingEl = document.querySelector(".loading");
 const imgEl = document.querySelector(".loading img");
 const citiesList = document.querySelector(".cities__list");
 const cityInput = document.getElementById("city__input");
-
-// setTimeout(() => {
-//   loadingEl.classList.add("anime");
-//   imgEl.classList.add("anime");
-//   setTimeout(() => {
-//     document.body.removeChild(loadingEl);
-//   }, 600);
-// }, 1000);
+const searchContainer = document.querySelector(".search-container");
+const prayerLocationInfo = document.querySelector(".prayer__location-info");
+const prayerInfoContainer = document.querySelector(".prayer__info");
+const loader = document.querySelector(".loader");
+setTimeout(() => {
+  loadingEl.classList.add("anime");
+  imgEl.classList.add("anime");
+  setTimeout(() => {
+    document.body.removeChild(loadingEl);
+  }, 600);
+}, 1000);
 
 const filterAndDisplayCities = (inputValue) => {
   axios.get("../assets/data/cities.json").then((response) => {
@@ -23,11 +26,12 @@ const filterAndDisplayCities = (inputValue) => {
     if (sortedCities.length > 0) {
       for (const city of sortedCities) {
         const content = `
-      <li class="city" data-city-name="${city.en}">${city.ar}</li>
+      <li class="city" onclick="getPrayerinfo('${city.ar}','${city.en}')">${city.ar}</li>
       `;
         citiesList.innerHTML += content;
       }
     } else {
+      citiesList.innerHTML = "";
       citiesList.innerHTML += "<p>لا يوجد مدينة بهذا الاسم</p>";
     }
   });
@@ -39,3 +43,46 @@ cityInput.addEventListener("input", (event) => {
   const inputValue = event.target.value.toLowerCase().trim();
   filterAndDisplayCities(inputValue);
 });
+
+const displayPrayeInfo = () => {};
+const getPrayerinfo = (cityNameAr, cityNameEn) => {
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      cityInput.value = cityNameAr;
+      loader.classList.add("show");
+      citiesList.parentElement.classList.remove("fill");
+      resolve();
+    }, 100);
+  })
+    .then(() => {
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          searchContainer.classList.add("animate__fadeOutUp");
+          resolve();
+        }, 1000);
+      });
+    })
+    .then(() => {
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          searchContainer.classList.add("hide");
+          resolve();
+        }, 1200);
+      });
+    })
+    .then(() => {
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          prayerLocationInfo.classList.add("show", "animate__fadeInUp");
+          resolve();
+        }, 1200);
+      });
+    })
+    .then(() => {
+      setTimeout(() => {
+        prayerInfoContainer.classList.add("show", "animate__fadeInUpBig");
+      }, 1200);
+    });
+
+  console.log(cityNameEn);
+};
