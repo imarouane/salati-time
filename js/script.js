12ce16941351ba82365b9c5d5871280e7d236fa9
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2023-08-14 17:20:34 +0100

    add getFormattedTodayDate Function to get exact prayer times

--- js/script.js
+++ js/script.js
@@ -6,14 +6,36 @@ const searchContainer = document.querySelector(".search-container");
 const prayerLocationInfo = document.querySelector(".prayer__location-info");
 const prayerInfoContainer = document.querySelector(".prayer__info");
 const loader = document.querySelector(".loader");
-setTimeout(() => {
-  loadingEl.classList.add("anime");
-  imgEl.classList.add("anime");
-  setTimeout(() => {
-    document.body.removeChild(loadingEl);
-  }, 600);
-}, 1000);
-
+let cityNameEl = document.querySelector(".city-name");
+// setTimeout(() => {
+//   loadingEl.classList.add("anime");
+//   imgEl.classList.add("anime");
+//   setTimeout(() => {
+//     document.body.removeChild(loadingEl);
+//   }, 600);
+// }, 1000);
+const arabic_months = [
+  "يناير",
+  "فبراير",
+  "مارس",
+  "أبريل",
+  "ماي",
+  "يونيو",
+  "يوليوز",
+  "غشت",
+  "سبتمبر",
+  "أكتوبر",
+  "نونبر",
+  "دجنبر",
+];
+const arabic_times = {
+  Fajr: "الفجر",
+  Sunrise: "الشروق",
+  Dhuhr: "الظهر",
+  Asr: "العصر",
+  Maghrib: "المغرب",
+  Isha: "العشاء",
+};
 const filterAndDisplayCities = (inputValue) => {
   axios.get("../assets/data/cities.json").then((response) => {
     const cities = response.data;
@@ -44,9 +66,39 @@ cityInput.addEventListener("input", (event) => {
   filterAndDisplayCities(inputValue);
 });
 
-const displayPrayeInfo = () => {};
+function getFormattedTodayDate() {
+  const today = new Date();
+  const day = String(today.getDate()).padStart(2, '0');
+  const month = String(today.getMonth() + 1).padStart(2, '0'); // 
+  const year = today.getFullYear();
+
+  return `${day}-${month}-${year}`;
+}
+
+const displayPrayeInfo = (cityNameEn) => {
+  axios
+    .get(
+      `http://api.aladhan.com/v1/calendarByCity/2023/8?city=${cityNameEn}&country=morocco&method=5`
+    )
+    .then(function (response) {
+      const allDataObjs = response.data.data;
+      const timings = [];
+      let today = getFormattedTodayDate();
+      for (const dataObj of allDataObjs) {
+        // timings.push(dataObj.timings)
+        if (dataObj.date.gregorian.date == today) {
+          console.log(today);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+displayPrayeInfo("Marrakech");
+
 const getPrayerinfo = (cityNameAr, cityNameEn) => {
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       cityInput.value = cityNameAr;
       loader.classList.add("show");
@@ -55,34 +107,44 @@ const getPrayerinfo = (cityNameAr, cityNameEn) => {
     }, 100);
   })
     .then(() => {
-      new Promise((resolve, reject) => {
+      return new Promise((resolve, reject) => {
         setTimeout(() => {
-          searchContainer.classList.add("animate__fadeOutUp");
+          searchContainer.classList.add(
+            "animate__animated",
+            "animate__fadeOutUp"
+          );
           resolve();
         }, 1000);
       });
     })
     .then(() => {
-      new Promise((resolve, reject) => {
+      return new Promise((resolve, reject) => {
         setTimeout(() => {
           searchContainer.classList.add("hide");
           resolve();
-        }, 1200);
+        }, 500);
       });
     })
     .then(() => {
-      new Promise((resolve, reject) => {
+      return new Promise((resolve, reject) => {
         setTimeout(() => {
-          prayerLocationInfo.classList.add("show", "animate__fadeInUp");
+          prayerLocationInfo.classList.add(
+            "show",
+            "animate__animated",
+            "animate__fadeInUp"
+          );
+          cityNameEl.textContent = cityNameAr;
           resolve();
-        }, 1200);
+        }, 100);
       });
     })
     .then(() => {
       setTimeout(() => {
-        prayerInfoContainer.classList.add("show", "animate__fadeInUpBig");
-      }, 1200);
+        prayerInfoContainer.classList.add(
+          "show",
+          "animate__animated",
+          "animate__fadeInUp"
+        );
+      }, 50);
     });
-
-  console.log(cityNameEn);
 };
